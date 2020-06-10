638fa9986fad793a08f3fd9ef5cfe1a49a5f162c
I518703 <sreevishnu.a.b@sap.com>
2020-06-10 15:52:59 +0530

    inference endpoint, add data and retrain endpoints

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from model import Model
+import logging
+import os
+
+port = int(os.environ.get('PORT', 3000))
+logging.basicConfig(level='INFO')
+
+app = Flask(__name__)
+model = Model.get_instance()
+
+@app.route('/predict', methods=['POST'])
+def predict():
+  data = request.get_json()
+  # logging.info(data)
+  prediction = model.predict(data)
+  logging.info(prediction)
+  return jsonify({"predicted price": prediction.tolist()})
+
+@app.route('/update-model', methods=['POST'])
+def update_model():
+  data = request.get_json()
+  logging.info(f"data {data}")
+  model.add_data(data)
+  return jsonify({"response": "model updated"})
+  # return jsonify({"error": "model could not be updated"}), 500
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=port)
+  
---
+++ model.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import logging
+
+class Model():
+  __instance = None
+
+  @staticmethod
+  def get_instance():
+    if Model.__instance == None:
+      Model.__instance = Model()
+      Model.__instance.fit()
+    return Model.__instance
+
+  def __init__(self):
+    self.model = None
+
+  def fit(self):
+    file_path = './melb_data.csv'
+    data = pd.read_csv(file_path)
+    logging.info(f"Length of dataset {len(data)}")
+    data = data.dropna(axis=0)
+    features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+    y = data.Price
+    X = data[features]
+    train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=1)
+    self.model = RandomForestRegressor(random_state=1)
+    self.model.fit(train_X, train_y)
+    # logging.info(self.model.predict(test_X))
+
+  def predict(self, data):
+    df = pd.DataFrame.from_dict(data, orient='columns')
+    prediction = self.model.predict(df)
+    # logging.info(prediction)
+    return prediction
+
+  def add_data(self, data):
+    df = pd.DataFrame.from_dict(data, orient='columns')
+    df.to_csv(path_or_buf='./melb_data.csv', mode='a',header=False)
+    self.fit()
+    return True
+
